6ccef0af29642c22db7489a8f5260d7b48840df3
nitesh <niteshnapit19990@gmail.com>
2020-09-17 22:05:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { templateJitUrl } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sms';
+
+  students:{name:string,age:string}[]= [];
+  student:{name:string,age:string}={name:'',age:''}
+  updateForm:{name:string,age:string}={name:'',age:''}
+  selectedStudentIndex
+
+  addStudent(){
+    // let temp = {name:this.student.name,age:this.student.age}
+    this.students.push({...this.student})
+    this.student = {name:'',age:''}
+    console.log(this.students)
+  }
+
+  delete(i){
+    this.students.splice(i,1)
+  }
+
+  onUpdate(i)
+  {
+    this.selectedStudentIndex = i
+    this.updateForm = {...this.students[i]}
+  }
+
+  update()
+  {
+    this.students[this.selectedStudentIndex] = {...this.updateForm}
+    this.selectedStudentIndex = null
+
+  }
 }
+
